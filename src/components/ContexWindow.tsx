edacbec718abb9b48094bb4ce55b48df32754374
SamnangGit <phengsamnanggit@gmail.com>
2024-07-01 12:25:42 +0700

    adding tailwind css

---
+++ src/components/ContexWindow.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ContexWindow() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">ContexWindow</h1>
+    </div>
+  );
+}
+
+export default ContexWindow;
